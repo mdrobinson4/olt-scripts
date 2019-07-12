3bc4ac7f4a6ce68421a58a138dd88c270c8e9b1b
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-07-11 23:25:54 -0500

    Update assetPanda.py

--- assetPanda.py
+++ assetPanda.py
@@ -1,11 +1,11 @@
-import asyncio
-from pyppeteer import launch
+import time
+from selenium import webdriver
 
-async def main():
-    browser = await launch(headless=False)
-    page = await browser.newPage()
-    await page.goto('http://example.com')
-    await page.screenshot({'path': 'example.png'})
-    await browser.close()
-
-asyncio.get_event_loop().run_until_complete(main())
+driver = webdriver.Chrome('chromedriver.exe')  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/xhtml');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
